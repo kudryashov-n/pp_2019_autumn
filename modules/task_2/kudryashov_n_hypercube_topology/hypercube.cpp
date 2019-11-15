3cb3dd721a0050d2f738801a091568e4f09077f0
Kudryashov Nikita <porpois60@gmail.com>
2019-11-16 01:35:13 +0300

    Tests re-write

--- modules/task_2/kudryashov_n_hypercube_topology/hypercube.cpp
+++ modules/task_2/kudryashov_n_hypercube_topology/hypercube.cpp
@@ -1,24 +1,25 @@
 // Copyright 2019 Kudryashov Nikita
-#include <mpi.h>
+
 #include "../../../modules/task_2/kudryashov_n_hypercube_topology/hypercube.h"
 
 MPI_Comm createHcube(unsigned int dim) {
-    if (dim == 0) {
-        throw "Dimensions of hypercube can't be equal to zero.";
-    }
-    int* dimSize = new int[dim];
-    int* periods = new int[dim];
-    for (unsigned int i = 0; i < dim; i++) {
-        dimSize[i] = 2;
-        periods[i] = 0;
-    }
-
     MPI_Comm hypercubeComm;
-    MPI_Cart_create(MPI_COMM_WORLD, dim, dimSize, periods, 1, &hypercubeComm);
 
-    delete[] dimSize;
-    delete[] periods;
+    if (dim != 0) {
+        int* dimSize = new int[dim];
+        int* periods = new int[dim];
+        for (unsigned int i = 0; i < dim; i++) {
+            dimSize[i] = 2;
+            periods[i] = 0;
+        }
 
-    return hypercubeComm;
+        MPI_Cart_create(MPI_COMM_WORLD, dim, dimSize, periods, 1, &hypercubeComm);
+
+        delete[] dimSize;
+        delete[] periods;
+
+        return hypercubeComm;
+    } else {
+        return MPI_COMM_NULL;
+    }
 }
-
--- modules/task_2/kudryashov_n_hypercube_topology/main.cpp
+++ modules/task_2/kudryashov_n_hypercube_topology/main.cpp
@@ -3,89 +3,83 @@
 #include <gtest/gtest.h>
 #include "./hypercube.h"
 
-TEST(HCubeTopology, can_create_hcube_dim_1) {
+TEST(HCubeTopology, can_create_hcube) {
     MPI_Comm Hcube;
-    int rank;
+    int size;
+    unsigned int dim;
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    ASSERT_NO_THROW(Hcube = createHcube(1));
-    MPI_Comm_rank(Hcube, &rank);
-}
+    for (int i = 0; i < 32; i++) {
+        if (size & (1 << i)) {
+            dim = i;
+        }
+    }
 
-TEST(HCubeTopology, can_create_hcube_dim_2) {
-    MPI_Comm Hcube;
-    int rank;
-
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    ASSERT_NO_THROW(Hcube = createHcube(2));
-    MPI_Comm_rank(Hcube, &rank);
-}
-
-TEST(HCubeTopology, can_create_hcube_dim_3) {
-    MPI_Comm Hcube;
-    int rank;
-
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    ASSERT_NO_THROW(Hcube = createHcube(3));
-    MPI_Comm_rank(Hcube, &rank);
-}
-
-TEST(HCubeTopology, can_create_hcube_dim_4) {
-    MPI_Comm Hcube;
-    int rank;
-
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    ASSERT_NO_THROW(Hcube = createHcube(4));
-    MPI_Comm_rank(Hcube, &rank);
-}
-
-TEST(HCubeTopology, throw_with_zero_dim) {
-    MPI_Comm Hcube;
-    int rank;
-
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    ASSERT_ANY_THROW(Hcube = createHcube(0));
-    MPI_Comm_rank(Hcube, &rank);
+    ASSERT_NO_THROW(Hcube = createHcube(dim));
+    if (Hcube != MPI_COMM_NULL) {
+        MPI_Comm_size(Hcube, &size);
+    }
 }
 
 TEST(HCubeTopology, topology_check) {
     MPI_Comm Hcube;
-    int status;
+    int status, size;
+    unsigned int dim;
 
-    Hcube = createHcube(4);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    MPI_Topo_test(Hcube, &status);
-    ASSERT_EQ(status, MPI_CART);
-}
+    for (int i = 0; i < 32; i++) {
+        if (size & (1 << i)) {
+            dim = i;
+        }
+    }
 
-TEST(HCubeTopology, dims_4_param_dims_check) {
-    MPI_Comm Hcube;
-    int rank, dims;
+    Hcube = createHcube(dim);
 
-    Hcube = createHcube(4);
-
-    MPI_Comm_rank(Hcube, &rank);
-
-    if (rank == 0) {
-        MPI_Cartdim_get(Hcube, &dims);
-        ASSERT_EQ(dims, 4);
+    if (Hcube != MPI_COMM_NULL) {
+        MPI_Topo_test(Hcube, &status);
+        ASSERT_EQ(status, MPI_CART);
     }
 }
 
-TEST(HCubeTopology, dims_4_param_periods_check) {
+TEST(HCubeTopology, param_dims_check) {
     MPI_Comm Hcube;
-    int rank, dims;
+    int dims, size;
+    unsigned int dim;
 
-    Hcube = createHcube(4);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    MPI_Comm_rank(Hcube, &rank);
+    for (int i = 0; i < 32; i++) {
+        if (size & (1 << i)) {
+            dim = i;
+        }
+    }
 
-    if (rank == 0) {
+    Hcube = createHcube(dim);
+
+    if (Hcube != MPI_COMM_NULL) {
+        MPI_Cartdim_get(Hcube, &dims);
+        ASSERT_EQ(dims, dim);
+    }
+}
+
+TEST(HCubeTopology, param_periods_check) {
+    MPI_Comm Hcube;
+    int dims, size;
+    unsigned int dim_s;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    for (int i = 0; i < 32; i++) {
+        if (size & (1 << i)) {
+            dim_s = i;
+        }
+    }
+
+    Hcube = createHcube(dim_s);
+
+    if (Hcube != MPI_COMM_NULL) {
         MPI_Cartdim_get(Hcube, &dims);
 
         int* dim = new int[dims];
@@ -105,16 +99,24 @@ TEST(HCubeTopology, dims_4_param_periods_check) {
     }
 }
 
-TEST(HCubeTopology, dims_4_param_coords_check) {
+TEST(HCubeTopology, param_coords_check) {
     MPI_Comm Hcube;
-    int rank, dims;
+    int dims, size, rank;
+    unsigned int dim_s;
 
-    Hcube = createHcube(4);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    MPI_Comm_rank(Hcube, &rank);
+    for (int i = 0; i < 32; i++) {
+        if (size & (1 << i)) {
+            dim_s = i;
+        }
+    }
 
-    if (rank == 0) {
+    Hcube = createHcube(dim_s);
+
+    if (Hcube != MPI_COMM_NULL) {
         MPI_Cartdim_get(Hcube, &dims);
+        MPI_Comm_rank(Hcube, &rank);
 
         int* dim = new int[dims];
         int* period = new int[dims];
@@ -123,7 +125,7 @@ TEST(HCubeTopology, dims_4_param_coords_check) {
         bool check = true;
 
         MPI_Cart_get(Hcube, dims, dim, period, coords);
-        MPI_Cart_coords(Hcube, 0, dims, rank_coords);
+        MPI_Cart_coords(Hcube, rank, dims, rank_coords);
 
         for (int i = 0; i < dims; i++) {
             if (rank_coords[i] != coords[i]) {
@@ -135,15 +137,22 @@ TEST(HCubeTopology, dims_4_param_coords_check) {
     }
 }
 
-TEST(HCubeTopology, dims_4_param_dim_array_check) {
+TEST(HCubeTopology, param_dim_array_check) {
     MPI_Comm Hcube;
-    int rank, dims;
+    int dims, size;
+    unsigned int dim_s;
 
-    Hcube = createHcube(4);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    MPI_Comm_rank(Hcube, &rank);
+    for (int i = 0; i < 32; i++) {
+        if (size & (1 << i)) {
+            dim_s = i;
+        }
+    }
 
-    if (rank == 0) {
+    Hcube = createHcube(dim_s);
+
+    if (Hcube != MPI_COMM_NULL) {
         MPI_Cartdim_get(Hcube, &dims);
 
         int* dim = new int[dims];
@@ -165,24 +174,36 @@ TEST(HCubeTopology, dims_4_param_dim_array_check) {
 
 TEST(HCubeTopology, can_send_msg) {
     MPI_Comm Hcube;
-    int rank;
+    int rank, size;
+    unsigned int dim_s;
 
-    Hcube = createHcube(4);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    MPI_Comm_rank(Hcube, &rank);
-
-    if (rank == 0) {
-        int msg = 15;
-
-        MPI_Send(&msg, 1, MPI_INT, 5, 1, Hcube);
+    for (int i = 0; i < 32; i++) {
+        if (size & (1 << i)) {
+            dim_s = i;
+        }
     }
-    if (rank == 5) {
-        int msg, ret;
-        MPI_Status status;
 
-        ret = MPI_Recv(&msg, 1, MPI_INT, 0, 1, Hcube, &status);
+    Hcube = createHcube(dim_s);
+    if (Hcube != MPI_COMM_NULL) {
+        MPI_Comm_rank(Hcube, &rank);
+        MPI_Comm_size(Hcube, &size);
+        if (size > 1) {
+            if (rank == 0) {
+                int msg = 15;
 
-        ASSERT_TRUE(ret == MPI_SUCCESS && msg == 15);
+                MPI_Send(&msg, 1, MPI_INT, size - 1, 1, Hcube);
+            }
+            if (rank == size - 1) {
+                int msg, ret;
+                MPI_Status status;
+
+                ret = MPI_Recv(&msg, 1, MPI_INT, 0, 1, Hcube, &status);
+
+                ASSERT_TRUE(ret == MPI_SUCCESS && msg == 15);
+            }
+        }
     }
 }
 
